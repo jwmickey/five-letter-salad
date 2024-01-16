124e6be7792819c5e6866efb02a60b66b99c91a4
Jody Mickey <jody.mickey@gmail.com>
2024-01-15 21:17:35 -0500

    update vue and other deps, improve keyboard

--- src/components/KeyBoard.vue
+++ src/components/KeyBoard.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { Keys } from "@/utils/GameTypes";
-defineProps<{
+const props = defineProps<{
   wrongLetters: string[];
   lettersInWord: string[];
   correctLetters: string[];
@@ -12,24 +12,38 @@ const rows: string[][] = [
   "asdfghjkl".split(""),
   `${Keys.ENTER}zxcvbnm${Keys.BACKSPACE}`.split(""),
 ];
+
+/**
+ * 'bg-gray-800 text-gray-400': wrongLetters.includes(key),
+          'bg-inword-400 text-gray-800':
+            lettersInWord.includes(key) && !correctLetters.includes(key),
+          'bg-correct-600 text-gray-800': correctLetters.includes(key),
+ */
+
+function keyClasses(k: string): string {
+  if (props.correctLetters.includes(k)) {
+    return 'bg-correct-600 text-gray-800';
+  } else if (props.lettersInWord.includes(k)) {
+    return 'bg-inword-400 text-gray-800';
+  } else if (props.wrongLetters.includes(k)) {
+    return 'bg-gray-800 text-gray-400';
+  } else {
+    return 'bg-gray-500 text-white';
+  }
+}
 </script>
 
 <template>
   <div class="flex flex-col">
     <div
-      class="row mb-1 flex flex-auto justify-center items-center self-center"
+      class="row mb-1 flex flex-auto justify-center items-center self-center gap-1"
       v-bind:key="i"
       v-for="(row, i) in rows"
     >
       <div
         @click="emit('typeLetter', key)"
-        class="flex-none basis-8 rounded-sm py-2 mr-1 border bg-gray-500 border-gray-500 text-white text-center uppercase cursor-pointer"
-        :class="{
-          'bg-gray-800 text-gray-400': wrongLetters.includes(key),
-          'bg-inword-400 text-gray-800':
-            lettersInWord.includes(key) && !correctLetters.includes(key),
-          'bg-correct-600 text-gray-800': correctLetters.includes(key),
-        }"
+        class="flex-none basis-8 rounded-sm py-2 border border-gray-500 text-center uppercase cursor-pointer font-bold text-lg"
+        :class="keyClasses(key)"
         v-bind:key="key"
         v-for="key in row"
       >
